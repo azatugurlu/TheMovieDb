b5b6bf0dc046da8597967bd96ecfb66ff0ac6223
azat ugurlu <azatugurlu@gmail.com>
2019-12-09 13:03:23 +0300

    Models added

---
+++ app/src/main/java/com/example/themoviedb/model/GenericResponse.java
@@ -0,0 +1,61 @@
+package com.example.themoviedb.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericResponse<T> {
+    private int page;
+    private List<T> results = new ArrayList<>();
+    private int total_results;
+    private int total_pages;
+    private String status_message;
+    private int status_code;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public String getStatus_message() {
+        return status_message;
+    }
+
+    public void setStatus_message(String status_message) {
+        this.status_message = status_message;
+    }
+
+    public int getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(int status_code) {
+        this.status_code = status_code;
+    }
+}
---
+++ app/src/main/java/com/example/themoviedb/model/Movie.java
@@ -0,0 +1,159 @@
+package com.example.themoviedb.model;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.Objects;
+
+public class Movie {
+    private String poster_path;
+    private boolean adult;
+    private String overview;
+    private String release_date;
+    private int id;
+    private String original_title;
+    private String original_language;
+    private String title;
+    private double popularity;
+    private int vote_count;
+    private double vote_average;
+
+    public Movie(String poster_path, boolean adult, String overview, String release_date, int id,
+                 String original_title, String original_language, String title, double popularity,
+                 int vote_count, double vote_average) {
+        this.poster_path = poster_path;
+        this.adult = adult;
+        this.overview = overview;
+        this.release_date = release_date;
+        this.id = id;
+        this.original_title = original_title;
+        this.original_language = original_language;
+        this.title = title;
+        this.popularity = popularity;
+        this.vote_count = vote_count;
+        this.vote_average = vote_average;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return adult == movie.adult &&
+                id == movie.id &&
+                Double.compare(movie.popularity, popularity) == 0 &&
+                vote_count == movie.vote_count &&
+                Double.compare(movie.vote_average, vote_average) == 0 &&
+                Objects.equals(poster_path, movie.poster_path) &&
+                Objects.equals(overview, movie.overview) &&
+                Objects.equals(release_date, movie.release_date) &&
+                Objects.equals(original_title, movie.original_title) &&
+                Objects.equals(original_language, movie.original_language) &&
+                Objects.equals(title, movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(poster_path, adult, overview, release_date, id, original_title, original_language, title, popularity, vote_count, vote_average);
+    }
+
+    public static DiffUtil.ItemCallback<Movie> DIFF_CALLBACK = new DiffUtil.ItemCallback<Movie>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Movie oldItem, @NonNull Movie newItem) {
+            return (oldItem.getId() == newItem.getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Movie oldItem, @NonNull Movie newItem) {
+            return oldItem.equals(newItem);
+        }
+    };
+}
